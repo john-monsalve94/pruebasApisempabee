b453f9c5284f88ec90d436778e2e8b55b4c3e482
john monsalve <john.monsalve94@gmail.com>
2024-02-26 16:10:18 -0500

    se creo el menu de items que va tener la pagina

---
+++ lib/menu/menu_items.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems(
+      {required this.title,
+      required this.subtitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItems>[
+MenuItems(
+    title: 'colmenas',
+    subtitle: 'colmenas',
+    link: '/colmenas',
+    icon: Icons.smart_button_outlined
+    ),
+MenuItems(
+    title: 'graficas',
+    subtitle: 'graficas generales',
+    link: '/graficas',
+    icon: Icons.hive_outlined
+    )
+];
