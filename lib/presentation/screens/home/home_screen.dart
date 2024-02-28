6abb7efee69dc3d223582df9e564d7af601902fc
john monsalve <john.monsalve94@gmail.com>
2024-02-28 12:17:05 -0500

    se modico home con listBuilder

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:empabee/menu/menu_items.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,9 +8,29 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('colmenas y graficas'),
+        title: const Text('colmenas y graficas'),
         // centerTitle: true,
       ),
+      body: const _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView(
+      // { super.key,} no lo voy a usar por ser privado y unica referencia
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+          return ListTile(
+            title: Text(menuItem.title),
+            subtitle: Text(menuItem.subtitle),
+          );
+        });
   }
 }
